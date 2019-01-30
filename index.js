878d0fd862968c8b28cb1d83444dab7bb9c4dd74
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-30 19:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = "70"
 
 
 
@@ -7,4 +7,19 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+
+if (name === "Susan") {console.log("The name is correct")}
+
+if (name === "Susan") {
+  console.log("The name is correct")
+}
+
+if (name === "Susan") {
+  console.log("The name is correct")} else {
+    console.log("Expected the name to be Susan")
+}
+
+var index = require("./index.js")
+
+console.log(index.name)
 
